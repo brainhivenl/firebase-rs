a5a183097fd33fb37c3c2ce4b7a31a270a50bc58
Alwin <alwin@brainhive.nl>
2024-03-11 11:22:14 +0100

    fix: let paramaters implement tostring instead of u32

--- src/params.rs
+++ src/params.rs
@@ -2,9 +2,9 @@ use crate::constants::{
     END_AT, EQUAL_TO, EXPORT, FORMAT, LIMIT_TO_FIRST, LIMIT_TO_LAST, ORDER_BY, SHALLOW, START_AT,
 };
 use crate::Firebase;
+use itertools::Itertools;
 use std::collections::HashMap;
 use url::Url;
-use itertools::Itertools;
 
 #[derive(Debug)]
 pub struct Params {
@@ -20,16 +20,13 @@ impl Params {
         }
     }
 
-    pub fn set_params(&mut self) -> () {
+    pub fn set_params(&mut self) {
         for (k, v) in self.params.iter().sorted() {
             self.uri.query_pairs_mut().append_pair(k, v);
         }
     }
 
-    pub fn add_param<T>(&mut self, key: &str, value: T) -> &mut Self
-        where
-            T: ToString,
-    {
+    pub fn add_param(&mut self, key: &str, value: impl ToString) -> &mut Self {
         self.params.insert(key.to_string(), value.to_string());
         self.set_params();
 
@@ -40,23 +37,23 @@ impl Params {
         self.add_param(ORDER_BY, key)
     }
 
-    pub fn limit_to_first(&mut self, count: u32) -> &mut Params {
+    pub fn limit_to_first(&mut self, count: impl ToString) -> &mut Params {
         self.add_param(LIMIT_TO_FIRST, count)
     }
 
-    pub fn limit_to_last(&mut self, count: u32) -> &mut Params {
+    pub fn limit_to_last(&mut self, count: impl ToString) -> &mut Params {
         self.add_param(LIMIT_TO_LAST, count)
     }
 
-    pub fn start_at(&mut self, index: u32) -> &mut Params {
+    pub fn start_at(&mut self, index: impl ToString) -> &mut Params {
         self.add_param(START_AT, index)
     }
 
-    pub fn end_at(&mut self, index: u32) -> &mut Params {
+    pub fn end_at(&mut self, index: impl ToString) -> &mut Params {
         self.add_param(END_AT, index)
     }
 
-    pub fn equal_to(&mut self, value: u32) -> &mut Params {
+    pub fn equal_to(&mut self, value: impl ToString) -> &mut Params {
         self.add_param(EQUAL_TO, value)
     }
 
@@ -73,21 +70,26 @@ impl Params {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
+    use crate::params::Params;
     use std::collections::HashMap;
     use url::Url;
-    use crate::params::Params;
 
     #[test]
     fn check_params() {
         let mut params: HashMap<String, String> = HashMap::new();
         params.insert("param_1".to_owned(), "value_1".to_owned());
         params.insert("param_2".to_owned(), "value_2".to_owned());
-        let mut param = Params { uri: Url::parse("https://github.com/emreyalvac").unwrap(), params };
+        let mut param = Params {
+            uri: Url::parse("https://github.com/emreyalvac").unwrap(),
+            params,
+        };
         param.set_params();
 
-        assert_eq!(param.uri.as_str(), "https://github.com/emreyalvac?param_1=value_1&param_2=value_2")
+        assert_eq!(
+            param.uri.as_str(),
+            "https://github.com/emreyalvac?param_1=value_1&param_2=value_2"
+        )
     }
 }
